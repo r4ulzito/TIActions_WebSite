fb9ba50475baf2bc6e12c47425e1f2e546013e41
r4ulzito <raulsouza180802@gmail.com>
2022-01-23 22:46:00 -0300

    commit3

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// Scroll Effect
+
+$("#navbar a, .btn").on("click", function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $("html, body").animate(
+            {
+                scrollTop: $(hash).offset().top - 100
+            },
+            800
+        );
+    }
+});
+
+
+
+
+
